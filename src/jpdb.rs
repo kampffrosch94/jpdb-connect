99b4e9f859f8265b98adf56ccc22e48e690b1428
kampffrosch94 <kampffrosch94@github.com>
2022-09-04 22:40:44 +0200

    cargo fmt

--- src/jpdb.rs
+++ src/jpdb.rs
@@ -134,7 +134,9 @@ impl JPDBConnection {
                 }
                 if self.config.add_mined_sentences {
                     info!("Add custom sentence: {}", abs_url(detail_url));
-                    vocab.set_custom_sentence(&mut self.service, &s.sentence).await?;
+                    vocab
+                        .set_custom_sentence(&mut self.service, &s.sentence)
+                        .await?;
                 }
             } else {
                 if self.config.auto_add.is_some() || self.config.auto_forq {
@@ -191,15 +193,17 @@ impl VocabCard<'_> {
         let vocab_id = self.find_id()?;
         let VocabId { v, s, r } = vocab_id;
         let edit_sentence_url = format!("/edit-shown-sentence?v={}&s={}&r={}", v, s, r);
-        let payload: [(&str, &str); 2] = 
-            [("sentence", sentence), ("translation", "")];
+        let payload: [(&str, &str); 2] = [("sentence", sentence), ("translation", "")];
         let res = form_request(service, &edit_sentence_url, payload)
             .await
             .context("set custom sentence request")?;
         let status = res.status();
         if !status.is_success() && !status.is_redirection() {
             debug!("Error body: {}", res.text().await.unwrap_or_default());
-            return Err(anyhow!("Adding custom sentence failed, status: {}", status.as_u16()));
+            return Err(anyhow!(
+                "Adding custom sentence failed, status: {}",
+                status.as_u16()
+            ));
         }
         Ok(())
     }
